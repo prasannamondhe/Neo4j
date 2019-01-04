9a662c4f29a4f71fe36f7e1032f05cd258e0f67c
prasannamondhe <34103675+prasannamondhe@users.noreply.github.com>
2019-01-04 17:25:15 +0530

    Updated NewMatrix.java file

--- Neo4jExample/src/main/java/NewMatrix.java
+++ Neo4jExample/src/main/java/NewMatrix.java
@@ -1,17 +1,7 @@
 import java.io.File;
 import java.io.IOException;
-
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.traversal.Evaluators;
-import org.neo4j.graphdb.traversal.TraversalDescription;
-import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.io.fs.FileUtils;
 
 /**
@@ -22,8 +12,12 @@ public class NewMatrix {
     public enum RelTypes implements RelationshipType {
         NEO_NODE,
         KNOWS,
-        CODED_BY,
+        FRIEND,
         HELP;
+    }
+
+    public enum Labels implements Label {
+        USER,
     }
 
     private static final File MATRIX_DB = new File("target/matrix-new-db");
@@ -33,8 +27,7 @@ public class NewMatrix {
     public static void main(String[] args) throws IOException {
         NewMatrix matrix = new NewMatrix();
         matrix.setUp();
-        System.out.println(matrix.printMatrixHackers());
-        System.out.println(matrix.traversalPath());
+        matrix.printMatrixHackers();
         matrix.shutdown();
     }
 
@@ -51,151 +44,132 @@ public class NewMatrix {
 
     private void createNodespace() {
         try (Transaction tx = graphDb.beginTx()) {
-            // Create matrix node
+
             Node matrix = graphDb.createNode();
             matrixNodeId = matrix.getId();
+            matrix.setProperty("name", "Root Node");
 
-            // Create Neo
-            Node one = graphDb.createNode();
+            Node one = graphDb.createNode(Labels.USER);
             one.setProperty("name", "1");
 
-            // connect Neo/Thomas to the matrix node
-            matrix.createRelationshipTo(one, RelTypes.NEO_NODE);
-
-            Node two = graphDb.createNode();
+            Node two = graphDb.createNode(Labels.USER);
             two.setProperty("name", "2");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship1 = one.createRelationshipTo(two, RelTypes.CODED_BY);
-            relationship1.setProperty("relName", " CODED_BY ");
-
-
-            Node three = graphDb.createNode();
+            Node three = graphDb.createNode(Labels.USER);
             three.setProperty("name", "3");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship2 = two.createRelationshipTo(three, RelTypes.HELP);
-            relationship2.setProperty("relName", " HELP ");
-
-            Node four = graphDb.createNode();
+            Node four = graphDb.createNode(Labels.USER);
             four.setProperty("name", "4");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship3 = two.createRelationshipTo(four, RelTypes.CODED_BY);
-            relationship3.setProperty("relName", " CODED_BY ");
-
-
-            Node five = graphDb.createNode();
+            Node five = graphDb.createNode(Labels.USER);
             five.setProperty("name", "5");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship4 = four.createRelationshipTo(five, RelTypes.KNOWS);
-            relationship4.setProperty("relName", " KNOWS ");
-
-
-            Node six = graphDb.createNode();
+            Node six = graphDb.createNode(Labels.USER);
             six.setProperty("name", "6");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship5 = four.createRelationshipTo(six, RelTypes.HELP);
-            relationship5.setProperty("relName", " HELP ");
-
-            Node seven = graphDb.createNode();
+            Node seven = graphDb.createNode(Labels.USER);
             seven.setProperty("name", "7");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship6 = five.createRelationshipTo(seven, RelTypes.HELP);
-            relationship6.setProperty("relName", " HELP ");
-
-            Node eight = graphDb.createNode();
+            Node eight = graphDb.createNode(Labels.USER);
             eight.setProperty("name", "8");
 
-            // connect Neo/Thomas to the matrix node
-            Relationship relationship7 = five.createRelationshipTo(eight, RelTypes.CODED_BY);
-            relationship7.setProperty("relName", " CODED_BY ");
+            Node nine = graphDb.createNode(Labels.USER);
+            nine.setProperty("name", "9");
 
-//            Node nine = graphDb.createNode();
-//            nine.setProperty("name", "9");
-//
-//            // connect Neo/Thomas to the matrix node
-//            Relationship relationship8 = six.createRelationshipTo(nine, RelTypes.CODED_BY);
-//            relationship8.setProperty("relName", " CODED_BY ");
-//
-//            Node ten = graphDb.createNode();
-//            ten.setProperty("name", "10");
-//
-//            // connect Neo/Thomas to the matrix node
-//            Relationship relationship9 = six.createRelationshipTo(ten, RelTypes.KNOWS);
-//            relationship9.setProperty("relName", " KNOWS ");
+            Node ten = graphDb.createNode(Labels.USER);
+            ten.setProperty("name", "10");
 
+            Node eleven = graphDb.createNode(Labels.USER);
+            eleven.setProperty("name", "11");
+
+            Node twelve = graphDb.createNode(Labels.USER);
+            twelve.setProperty("name", "12");
+
+            Node thirteen = graphDb.createNode(Labels.USER);
+            thirteen.setProperty("name", "13");
+
+            Node fourteen = graphDb.createNode(Labels.USER);
+            fourteen.setProperty("name", "14");
+
+
+            matrix.createRelationshipTo(one, RelTypes.NEO_NODE);
+
+            Relationship relationship1 = one.createRelationshipTo(two, RelTypes.HELP);
+            relationship1.setProperty("relName", " HELP ");
+
+            Relationship relationship2 = one.createRelationshipTo(three, RelTypes.KNOWS);
+            relationship1.setProperty("relName", " KNOWS ");
+
+            Relationship relationship3 = two.createRelationshipTo(four, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship4 = two.createRelationshipTo(five, RelTypes.KNOWS);
+            relationship1.setProperty("relName", " KNOWS ");
+
+            Relationship relationship5 = three.createRelationshipTo(six, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship6 = three.createRelationshipTo(seven, RelTypes.KNOWS);
+            relationship1.setProperty("relName", " KNOWS ");
+
+            Relationship relationship7 = five.createRelationshipTo(eight, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship8 = six.createRelationshipTo(eight, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship9 = eight.createRelationshipTo(nine, RelTypes.HELP);
+            relationship1.setProperty("relName", " HELP ");
+
+            Relationship relationship10 = eight.createRelationshipTo(ten, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship11 = ten.createRelationshipTo(nine, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
+
+            Relationship relationship12 = nine.createRelationshipTo(eleven, RelTypes.HELP);
+            relationship1.setProperty("relName", " HELP ");
+
+            Relationship relationship13 = nine.createRelationshipTo(twelve, RelTypes.KNOWS);
+            relationship1.setProperty("relName", " KNOWS ");
+
+            Relationship relationship14 = twelve.createRelationshipTo(thirteen, RelTypes.HELP);
+            relationship1.setProperty("relName", " HELP ");
+
+            Relationship relationship15 = twelve.createRelationshipTo(fourteen, RelTypes.FRIEND);
+            relationship1.setProperty("relName", " FRIEND ");
 
             tx.success();
         }
     }
 
-    /**
-     * Get the Neo node. (a.k.a. Thomas Anderson node)
-     *
-     * @return the Neo node
-     */
-    private Node getNeoNode() {
-        return graphDb.getNodeById(matrixNodeId)
-                .getSingleRelationship(RelTypes.NEO_NODE, Direction.OUTGOING)
-                .getEndNode();
-    }
 
-
-    public String printMatrixHackers() {
+    public void printMatrixHackers() {
         try (Transaction tx = graphDb.beginTx()) {
-            String output = "Hackers:\n";
-            int numberOfHackers = 0;
-            Traverser traverser = findHackers(getNeoNode());
-            for (Path hackerPath : traverser) {
-
-                for (Relationship relationship : hackerPath.relationships()) {
-
-                    if (relationship.getType().name().contains(RelTypes.CODED_BY.name())) {
-                        output += "At depth " + hackerPath.length() + " => "
-                                + hackerPath.endNode()
-                                .getProperty("name") + "\n";
-                        numberOfHackers++;
-                        break;
-                    } else if (relationship.getType().name().contains(RelTypes.KNOWS.name())) {
-                        output += "At depth " + hackerPath.length() + " => "
-                                + hackerPath.endNode()
-                                .getProperty("name") + "\n";
-                        numberOfHackers++;
-                        break;
+            Node startNode = graphDb.findNode(Labels.USER,"name","1");
+            while(startNode.hasRelationship(Direction.OUTGOING)){
+                if(startNode.hasRelationship(Direction.OUTGOING,RelTypes.FRIEND)) {
+                    for (Relationship relationship : startNode.getRelationships(Direction.OUTGOING, RelTypes.FRIEND)) {
+                        try {
+                            Node endNode = relationship.getOtherNode(startNode);
+                            System.out.println(startNode.getProperties("name") + " -> " + endNode.getProperty("name"));
+                            startNode = endNode;
+                        }catch (NotFoundException e){
+                            System.out.println("No more relationships to endnode");
+                        }
                     }
-
+                }else if(startNode.hasRelationship(Direction.OUTGOING,RelTypes.KNOWS)){
+                    for (Relationship relationship : startNode.getRelationships(Direction.OUTGOING, RelTypes.KNOWS)) {
+                        try {
+                            Node endNode = relationship.getOtherNode(startNode);
+                            System.out.println(startNode.getProperties("name") + " -> " + endNode.getProperty("name"));
+                            startNode = endNode;
+                        }catch (NotFoundException e){
+                            System.out.println("No more relationships to endnode");
+                        }
+                    }
                 }
             }
-            output += "Number of hackers found: " + numberOfHackers + "\n";
-            return output;
-        }
-    }
-
-    private Traverser findHackers(final Node startNode) {
-        TraversalDescription td = graphDb.traversalDescription()
-                .depthFirst()
-                .relationships(RelTypes.CODED_BY, Direction.OUTGOING)
-                .relationships(RelTypes.KNOWS, Direction.OUTGOING);
-        //.evaluator(Evaluators.toDepth( 4 ));
-        return td.traverse(startNode);
-    }
-
-    public String traversalPath() {
-        try (Transaction tx = graphDb.beginTx()) {
-            String output = "";
-            for (Path position : graphDb.traversalDescription()
-                    .depthFirst()
-                    .relationships(RelTypes.CODED_BY, Direction.OUTGOING)
-                    .relationships(RelTypes.KNOWS, Direction.OUTGOING)
-                    .evaluator(Evaluators.toDepth(5))
-                    .traverse(getNeoNode())) {
-                output += position + "\n";
-            }
-            return output;
         }
     }
 
